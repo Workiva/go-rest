2c016a50af5baf48aaa7d801f2944bfa50775039
Tanner Miller <tanner.miller+github@webfilings.com>
2014-07-21 09:29:48 -0500

    Initial dump of go-rest code.
    Initial code commit of go-rest codebase. This is the REST API
    implementation in Go of the company standard REST API.
    
    This does need to be reviewed as it has changed quite a bit since it was
    wfrest.

---
+++ rest/client_test.go
@@ -0,0 +1,116 @@
+package rest
+
+import (
+	"fmt"
+	"math/rand"
+	"net/http"
+	"net/http/httptest"
+	"net/url"
+	"strconv"
+	"testing"
+	"time"
+)
+
+type testObj struct{ Prop string }
+
+type testConsumer struct{ key, secret string }
+
+// AuthorizeRequest will provide a testing specific authorization without
+// actually signing the request.
+func (tc testConsumer) AuthorizeRequest(urlStr string, requestType string, form url.Values) url.Values {
+	baseParams := map[string]string{
+		"oauth_consumer_key":     tc.key,
+		"oauth_signature_method": "HMAC-SHA1",
+		"oauth_timestamp":        strconv.FormatInt(time.Now().Unix(), 10),
+		"oauth_version":          "1.0",
+		"oauth_nonce":            strconv.FormatInt(rand.Int63(), 10),
+	}
+	for param, value := range baseParams {
+		form.Set(param, value)
+	}
+	result := requestType + "&" + url.QueryEscape(urlStr)
+	baseString := result + "&" + url.QueryEscape(form.Encode())
+	form.Set("oauth_signature", baseString)
+	return form
+}
+
+func TestEverything(t *testing.T) {
+	returnJson := `{"Status": 200, "Reason": "", "Messages": [], "Next": "", "Results": {"Prop": "a"}}`
+
+	formData := map[string]string{
+		"oauth_consumer_key":     "token",
+		"oauth_signature_method": "HMAC-SHA1",
+		"oauth_version":          "1.0",
+	}
+
+	ts := httptest.NewServer(http.HandlerFunc(func(
+		w http.ResponseWriter, r *http.Request) {
+
+		r.ParseForm()
+
+		for _, prop := range []string{"oauth_nonce", "oauth_timestamp"} {
+			if _, ok := r.Form[prop]; !ok {
+				t.Errorf("Form property %s not found", prop)
+			}
+		}
+
+		for key, value := range formData {
+			if r.Form[key][0] != value {
+				t.Errorf("Form data value %s, want %s", r.Form[key][0], value)
+			}
+		}
+
+		w.Header().Set("Content-Type", "application/json")
+		fmt.Fprintln(w, returnJson)
+	}))
+
+	defer ts.Close()
+
+	var c = Client{testConsumer{
+		"token",
+		"value",
+	}}
+
+	testObj := testObj{}
+	fs := []func() (*BaseResponse, error){
+		func() (*BaseResponse, error) {
+			return c.GetJson(ts.URL, nil, &testObj)
+		},
+		func() (*BaseResponse, error) {
+			return c.DeleteJson(ts.URL, nil, &testObj)
+		},
+		func() (*BaseResponse, error) {
+			return c.PutJson(ts.URL, nil, &testObj)
+		},
+		func() (*BaseResponse, error) {
+			return c.PostJson(ts.URL, nil, &testObj)
+		},
+	}
+
+	for _, f := range fs {
+		resp, err := f()
+		if err != nil {
+			t.Errorf("Error in request: %s\n", err)
+		}
+
+		if resp.Status != 200 {
+			t.Errorf("REST response status %s, want %s", resp.Status, 200)
+		}
+
+		if resp.Reason != "" {
+			t.Errorf("REST response reason %s, want %s", resp.Reason, "")
+		}
+
+		if len(resp.Messages) != 0 {
+			t.Errorf("REST response message count %s, want %s", len(resp.Messages), 0)
+		}
+
+		if resp.Next != "" {
+			t.Errorf("REST response next %s, want %s", resp.Next, "")
+		}
+
+		if testObj.Prop != "a" {
+			t.Errorf("testObj prop %s, want %s", testObj.Prop, "a")
+		}
+	}
+}
