bddb770465f29ebb1da852621fb2967b9b3e52d8
Tyler Treat <tyler.treat@webfilings.com>
2014-07-30 17:01:53 -0500

    Add unit tests for create handler

--- server/handler_test.go
+++ server/handler_test.go
@@ -1,6 +1,7 @@
 package server
 
 import (
+	"bytes"
 	"fmt"
 	"net/http"
 	"net/http/httptest"
@@ -28,7 +29,11 @@ func (m *MockResourceHandler) ResourceName() string {
 
 func (m *MockResourceHandler) CreateResource(r context.RequestContext, data map[string]interface{}) (interface{}, error) {
 	args := m.Mock.Called()
-	return args.Get(0).(*Resource), args.Error(1)
+	resource := args.Get(0)
+	if resource != nil {
+		resource = resource.(*Resource)
+	}
+	return resource, args.Error(1)
 }
 
 func (m *MockResourceHandler) ReadResource(r context.RequestContext, id string) (interface{}, error) {
@@ -48,6 +53,89 @@ func (m *MockResourceHandler) UpdateResource(r context.RequestContext, id string
 func (m *MockResourceHandler) DeleteResource(r context.RequestContext, id string) (interface{}, error) {
 	args := m.Mock.Called()
 	return args.Get(0).(*Resource), args.Error(1)
+}
+
+// Ensures that the create handler returns a Not Implemented code if an invalid response format is provided.
+func TestHandleCreateBadFormat(t *testing.T) {
+	assert := assert.New(t)
+	handler := new(MockResourceHandler)
+	router := mux.NewRouter()
+
+	handler.On("ResourceName").Return("foo")
+
+	RegisterResourceHandler(router, handler)
+	createHandler := router.Get("create").GetHandler()
+
+	payload := []byte(`{"foo": "bar"}`)
+	r := bytes.NewReader(payload)
+	req, _ := http.NewRequest("POST", "http://foo.com/api/v0.1/foo?format=blah", r)
+	resp := httptest.NewRecorder()
+
+	createHandler.ServeHTTP(resp, req)
+
+	handler.Mock.AssertExpectations(t)
+	assert.Equal(http.StatusNotImplemented, resp.Code, "Incorrect response code")
+	assert.Equal(
+		`{"error":"Format not implemented: blah","success":false}`,
+		resp.Body.String(),
+		"Incorrect response string",
+	)
+}
+
+// Ensures that the create handler returns an Internal Server Error code when the createFunc returns an error.
+func TestHandleCreateBadCreate(t *testing.T) {
+	assert := assert.New(t)
+	handler := new(MockResourceHandler)
+	router := mux.NewRouter()
+
+	handler.On("ResourceName").Return("foo")
+	handler.On("CreateResource").Return(nil, fmt.Errorf("couldn't create"))
+
+	RegisterResourceHandler(router, handler)
+	createHandler := router.Get("create").GetHandler()
+
+	payload := []byte(`{"foo": "bar"}`)
+	r := bytes.NewReader(payload)
+	req, _ := http.NewRequest("POST", "http://foo.com/api/v0.1/foo", r)
+	resp := httptest.NewRecorder()
+
+	createHandler.ServeHTTP(resp, req)
+
+	handler.Mock.AssertExpectations(t)
+	assert.Equal(http.StatusInternalServerError, resp.Code, "Incorrect response code")
+	assert.Equal(
+		`{"error":"couldn't create","success":false}`,
+		resp.Body.String(),
+		"Incorrect response string",
+	)
+}
+
+// Ensures that the create handler returns the serialized resource and Created code when createFunc succeeds.
+func TestHandleCreateHappyPath(t *testing.T) {
+	assert := assert.New(t)
+	handler := new(MockResourceHandler)
+	router := mux.NewRouter()
+
+	handler.On("ResourceName").Return("foo")
+	handler.On("CreateResource").Return(&Resource{Foo: "bar"}, nil)
+
+	RegisterResourceHandler(router, handler)
+	createHandler := router.Get("create").GetHandler()
+
+	payload := []byte(`{"foo": "bar"}`)
+	r := bytes.NewReader(payload)
+	req, _ := http.NewRequest("POST", "http://foo.com/api/v0.1/foo", r)
+	resp := httptest.NewRecorder()
+
+	createHandler.ServeHTTP(resp, req)
+
+	handler.Mock.AssertExpectations(t)
+	assert.Equal(http.StatusCreated, resp.Code, "Incorrect response code")
+	assert.Equal(
+		`{"result":{"foo":"bar"},"success":true}`,
+		resp.Body.String(),
+		"Incorrect response string",
+	)
 }
 
 // Ensures that the read handler returns a Not Implemented code if an invalid response format is provided.
@@ -71,7 +159,7 @@ func TestHandleReadBadFormat(t *testing.T) {
 	assert.Equal(
 		`{"error":"Format not implemented: blah","success":false}`,
 		resp.Body.String(),
-		"Incorrect error string",
+		"Incorrect response string",
 	)
 }
 
@@ -97,7 +185,7 @@ func TestHandleReadBadRead(t *testing.T) {
 	assert.Equal(
 		`{"error":"no resource","success":false}`,
 		resp.Body.String(),
-		"Incorrect error string",
+		"Incorrect response string",
 	)
 }
 
@@ -123,6 +211,6 @@ func TestHandleReadHappyPath(t *testing.T) {
 	assert.Equal(
 		`{"result":{"foo":"hello"},"success":true}`,
 		resp.Body.String(),
-		"Incorrect error string",
+		"Incorrect response string",
 	)
 }
