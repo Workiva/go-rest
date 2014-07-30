e8fe0a3677adf1df713fd2b06e8e66c70260113a
Tyler Treat <tyler.treat@webfilings.com>
2014-07-30 17:19:53 -0500

    Add unit tests for delete handler

--- server/handler_test.go
+++ server/handler_test.go
@@ -56,7 +56,11 @@ func (m *MockResourceHandler) UpdateResource(r context.RequestContext, id string
 
 func (m *MockResourceHandler) DeleteResource(r context.RequestContext, id string) (interface{}, error) {
 	args := m.Mock.Called()
-	return args.Get(0).(*Resource), args.Error(1)
+	resource := args.Get(0)
+	if resource != nil {
+		resource = resource.(*Resource)
+	}
+	return resource, args.Error(1)
 }
 
 // Ensures that the create handler returns a Not Implemented code if an invalid response format is provided.
@@ -297,6 +301,83 @@ func TestHandleUpdateHappyPath(t *testing.T) {
 	assert.Equal(http.StatusOK, resp.Code, "Incorrect response code")
 	assert.Equal(
 		`{"result":{"foo":"bar"},"success":true}`,
+		resp.Body.String(),
+		"Incorrect response string",
+	)
+}
+
+// Ensures that the delete handler returns a Not Implemented code if an invalid response format is provided.
+func TestHandleDeleteBadFormat(t *testing.T) {
+	assert := assert.New(t)
+	handler := new(MockResourceHandler)
+	router := mux.NewRouter()
+
+	handler.On("ResourceName").Return("foo")
+
+	RegisterResourceHandler(router, handler)
+	deleteHandler := router.Get("delete").GetHandler()
+
+	req, _ := http.NewRequest("DELETE", "http://foo.com/api/v0.1/foo/1?format=blah", nil)
+	resp := httptest.NewRecorder()
+
+	deleteHandler.ServeHTTP(resp, req)
+
+	handler.Mock.AssertExpectations(t)
+	assert.Equal(http.StatusNotImplemented, resp.Code, "Incorrect response code")
+	assert.Equal(
+		`{"error":"Format not implemented: blah","success":false}`,
+		resp.Body.String(),
+		"Incorrect response string",
+	)
+}
+
+// Ensures that the delete handler returns an Internal Server Error code when the deleteFunc returns an error.
+func TestHandleDeleteBadDelete(t *testing.T) {
+	assert := assert.New(t)
+	handler := new(MockResourceHandler)
+	router := mux.NewRouter()
+
+	handler.On("ResourceName").Return("foo")
+	handler.On("DeleteResource").Return(nil, fmt.Errorf("no resource"))
+
+	RegisterResourceHandler(router, handler)
+	deleteHandler := router.Get("delete").GetHandler()
+
+	req, _ := http.NewRequest("DELETE", "http://foo.com/api/v0.1/foo/1", nil)
+	resp := httptest.NewRecorder()
+
+	deleteHandler.ServeHTTP(resp, req)
+
+	handler.Mock.AssertExpectations(t)
+	assert.Equal(http.StatusInternalServerError, resp.Code, "Incorrect response code")
+	assert.Equal(
+		`{"error":"no resource","success":false}`,
+		resp.Body.String(),
+		"Incorrect response string",
+	)
+}
+
+// Ensures that the delete handler returns the serialized resource and OK code when deleteFunc succeeds.
+func TestHandleDeleteHappyPath(t *testing.T) {
+	assert := assert.New(t)
+	handler := new(MockResourceHandler)
+	router := mux.NewRouter()
+
+	handler.On("ResourceName").Return("foo")
+	handler.On("DeleteResource").Return(&Resource{Foo: "hello"}, nil)
+
+	RegisterResourceHandler(router, handler)
+	deleteHandler := router.Get("delete").GetHandler()
+
+	req, _ := http.NewRequest("DELETE", "http://foo.com/api/v0.1/foo/1", nil)
+	resp := httptest.NewRecorder()
+
+	deleteHandler.ServeHTTP(resp, req)
+
+	handler.Mock.AssertExpectations(t)
+	assert.Equal(http.StatusOK, resp.Code, "Incorrect response code")
+	assert.Equal(
+		`{"result":{"foo":"hello"},"success":true}`,
 		resp.Body.String(),
 		"Incorrect response string",
 	)
