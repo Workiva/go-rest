a9869bbb71943fba480b1fb67713ecc044157635
Tyler Treat <tyler.treat@webfilings.com>
2014-08-06 13:30:52 -0500

    Rename rest package to client

--- client/client.go
+++ client/client.go
@@ -1,5 +1,5 @@
 /*
-Package rest contains the company standard REST API client implementation.
+Package client contains the company standard REST API client implementation.
 
 This package can be used with any type that implements the Consumer interface:
 
@@ -14,7 +14,7 @@ This package can be used with any type that implements the Consumer interface:
 
     resp, err := rc.GetJson("http://example.com/api/", params, nil)
 */
-package rest
+package client
 
 import (
 	"encoding/json"
