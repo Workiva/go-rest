817dfff801cccb5381d4016e1eceafe1914ffc22
Tyler Treat <tyler.treat@webfilings.com>
2014-08-06 17:20:56 -0500

    Fix import ordering

--- rest/api_test.go
+++ rest/api_test.go
@@ -7,10 +7,10 @@ import (
 	"net/http/httptest"
 	"testing"
 
-	"go-rest/rest/context"
-
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/mock"
+
+	"go-rest/rest/context"
 )
 
 type TestResource struct {
