34681c4edd0264029452cc8ffc9bee6c18a5bd9b
Alexander Campbell <alexander.campbell@workiva.com>
2015-03-09 11:10:02 -0500

    Wrap example docstrings to 80 characters
    For consistency and improved Godoc rendering.

--- rest/example_ids_only_test.go
+++ rest/example_ids_only_test.go
@@ -18,34 +18,36 @@ package rest
 
 import "strconv"
 
-// MyResource represents a domain model for which we want to perform CRUD operations with.
-// Endpoints can operate on any type of entity -- primitive, struct, or composite -- so long
-// as it is serializable (by default, this means JSON-serializable via either MarshalJSON
-// or JSON struct tags).
+// MyResource represents a domain model for which we want to perform CRUD
+// operations with. Endpoints can operate on any type of entity -- primitive,
+// struct, or composite -- so long as it is serializable (by default, this means
+// JSON-serializable via either MarshalJSON or JSON struct tags).
 type MyResource struct {
 	ID     int    `json:"id"`
 	Foobar string `json:"foobar"`
 }
 
-// MyResourceHandler implements the ResourceHandler interface. It specifies the business
-// logic for performing CRUD operations. BaseResourceHandler provides stubs for each method
-// if you only need to implement certain operations (as this example illustrates).
+// MyResourceHandler implements the ResourceHandler interface. It specifies the
+// business logic for performing CRUD operations. BaseResourceHandler provides
+// stubs for each method if you only need to implement certain operations (as
+// this example illustrates).
 type MyResourceHandler struct {
 	BaseResourceHandler
 }
 
-// ResourceName is used to identify what resource a handler corresponds to and is used
-// in the endpoint URLs, i.e. /api/:version/myresource.
+// ResourceName is used to identify what resource a handler corresponds to and
+// is used in the endpoint URLs, i.e. /api/:version/myresource.
 func (m MyResourceHandler) ResourceName() string {
 	return "myresource"
 }
 
-// ReadResourceList is the logic that corresponds to reading multiple resources, perhaps
-// with specified query parameters accessed through the RequestContext. This is
-// mapped to GET /api/:version/myresource. Typically, this would make some sort of database
-// query to fetch the resources. It returns the slice of results, a cursor (or empty) string,
-// and error (or nil). In this example, we illustrate how to use a query parameter to
-// return only the IDs of our resources.
+// ReadResourceList is the logic that corresponds to reading multiple resources,
+// perhaps with specified query parameters accessed through the RequestContext.
+// This is mapped to GET /api/:version/myresource. Typically, this would make
+// some sort of database query to fetch the resources. It returns the slice of
+// results, a cursor (or empty) string, and error (or nil). In this example, we
+// illustrate how to use a query parameter to return only the IDs of our
+// resources.
 func (m MyResourceHandler) ReadResourceList(ctx RequestContext, limit int,
 	cursor string, version string) ([]Resource, string, error) {
 	// Make a database call here.
@@ -67,8 +69,8 @@ func (m MyResourceHandler) ReadResourceList(ctx RequestContext, limit int,
 	return resources, "", nil
 }
 
-// This example shows how to implement a ResourceHandler which has an endpoint for
-// fetching multiple resources and, optionally, retrieving only their IDs.
+// This example shows how to implement a ResourceHandler which has an endpoint
+// for fetching multiple resources and, optionally, retrieving only their IDs.
 func Example_idsOnly() {
 	api := NewAPI(NewConfiguration())
 
