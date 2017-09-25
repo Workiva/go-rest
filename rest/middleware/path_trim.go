dedca6853607122e5222aa539ee84474e24a1bf2
chriskempf-wf <chris.kempf@workiva.com>
2017-09-25 13:57:43 -0500

    Minor rewording of docstring

--- rest/middleware/path_trim.go
+++ rest/middleware/path_trim.go
@@ -8,8 +8,8 @@ import(
 )
 
 // NewPathTrimMiddleware returns a Middleware which inspects request paths
-// and removes the content of trimPortion from them. With a path of /foo/bar/
-// and a trimpPortion of /foo you would be left with a path of /bar/.
+// and removes the content of trimPortion from the prefix. With a path of /foo/bar
+// and a trimpPortion of /foo you would be left with a path of /bar.
 func NewPathTrimMiddleware(trimPortion string) rest.Middleware {
 	return func(w http.ResponseWriter, r *http.Request) *rest.MiddlewareError {
 		r.URL.Path = strings.TrimPrefix(r.URL.Path, trimPortion)
